2d7e8d6cd1d119afe8a625784311f4875626b194
lanre dagogo <lanredagogo23@gmail.com>
2024-04-05 10:58:13 +0100

    adding script

---
+++ index.js/arrays.js
@@ -0,0 +1,59 @@
+//Arrays basics
+     /* let favoriteFood = ["rice", "beans", "yam", "fufu", "eba"];
+      console.log(favoriteFood[1]);
+      favoriteFood[4] = "indomie";
+      console.log(favoriteFood);
+      favoriteFood.shift(0);
+      console.log(favoriteFood);
+      let formerFavoriteFood = "rice";
+      console.log(formerFavoriteFood);
+      favoriteFood.unshift("spaghetti");
+      console.log(favoriteFood);
+      favoriteFood.push("bread");
+      console.log(favoriteFood);
+      let empty = [];
+      empty.pop(2);
+      console.log(empty);
+      let number = [2, 3, 4, 5];
+      number.splice(1, 1);
+      console.log(number);
+      let name = ["alpha", "gamma", "delta"];
+      name.splice(1, "beta", "beta");
+      console.log(name);
+      let figure = [10, -10, -5, -3, 2, 1];
+      figure.splice(0, 4, 10, 9, 8, 7, 6, 5, 4, 3);
+      console.log(figure); */
+
+      // Arrays Method Part 1
+      /* let arr = ["Lanre"];
+      arr.push("Akinbote");
+      arr.unshift("yellow");
+      arr;
+      arr.shift(0);
+      let arr2 = [30];
+      arr2.push("JavaScript");
+      arr2;
+      arr2.indexOf(30);
+      let combinedArr = arr.concat(arr2);
+      combinedArr; */
+
+      // Arrays Method Part 2
+      let arr = ["Javascript", "Python", "Ruby", "Java"];
+      let arr2 = arr.slice("Python", "Ruby");
+      let combinedArr = arr.concat(arr2);
+      combinedArr
+      /* "value" is passed to any data types, while "references" is an object that can be passed to any data types. i understand the useage of both. however i do not know how best to explain it. */
+
+      // Arrays Method Part 3
+      // 1. yes i know how to create an Array.
+      // 2. "unshift()"
+      // 3. "push()"
+      // 4. "pop()"
+      // 5. "shift()"
+      // 6. "splice()"
+      // 7. "splice(.........)"
+      // 8. "include" function is used to an element to an Array. arr.[3] = "lanre";
+      // 9. the "indexOf" function is use to determine the index number of an elemenmt in an Array.
+      // 10. [] === [] will return false because it is a falsely value.
+
+      

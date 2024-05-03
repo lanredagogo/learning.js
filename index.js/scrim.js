7441c81db02908cf292ca638a42a80085ea0e3d6
lanre dagogo <lanredagogo23@gmail.com>
2024-05-03 11:30:43 +0100

    adding my first javascript website with scrimba

---
+++ index.js/scrim.js
@@ -0,0 +1,90 @@
+/*let myAge = 30
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
+let bonusCount = 50
+bonusCount = bonusCount + 50
+bonusCount = bonusCount - 75
+bonusCount = bonusCount + 45
+console.log(bonusCount)*/
+
+// Lesson 1
+let people = ["Greg", "Mary", "Devon", "James"];
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i]);
+}
+people.shift()
+people.pop()
+people.unshift("Matt")
+people.push("Dagogo")
+for (let i = 0; i < 2; i++) {
+    console.log(people[i]);
+}
+people.slice(2,3);
+people.indexOf(1);
+people.indexOf("foo");
+people
+people.splice(2,1, "Elizabeth", "Artie")
+let withBob = people.concat("Bob")
+withBob
+people
+
+
+// Lesson 2
+let obj = {
+    firstName: "Lanre",
+    lastName: "Dagogo",
+    occupation: "Developer"
+}
+obj.firstName
+obj.lastName
+obj.occupation
+obj.hobby = "Reading"
+delete obj.occupation
+
+
+// Lesson 3 
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "https://www.devtopics.com/best-programming-jokes/"
+};
+
+programming.languages.push("Go");
+programming.difficulty = 7;
+delete programming.jokes
+programming.isFun = true
+for (let i = 0; i < programming.languages.length; i++) {
+    console.log(programming.languages[i])
+    console.log(programming)
+}
+
+function myName()  {
+    let fullName = "Lanre Adebayo Dagogo"
+    console.log(fullName)
+}
+myName()
+
+let favoriteFood = ["Rice", "Beans"]
+
+function randomFood() {
+    let bestFood = Math.floor(Math.random() * favoriteFood.length)
+    console.log(favoriteFood[bestFood])
+}
+
+randomFood()
+
+
+/*function map(array, callback) {
+    let results = array.concat(callback)
+    for (let i = 0; i < callback.length; i++) {
+        //return callback[i]
+        console.log(callback[i])
+    }
+}
+
+map()*/
